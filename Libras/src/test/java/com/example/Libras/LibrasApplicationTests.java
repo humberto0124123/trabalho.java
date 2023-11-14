96248a2f9d8c1c86e79e31741448c4a802aa3e96
humberto0124123 <150842260+humberto0124123@users.noreply.github.com>
2023-11-14 15:02:48 -0300

    Add files via upload

---
+++ Libras/src/test/java/com/example/Libras/LibrasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Libras;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibrasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
